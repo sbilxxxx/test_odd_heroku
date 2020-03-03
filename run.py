250320600559f6575cd49e84797676ac378751cc
fukamizu-i <sotobenjamin0307@gmail.com>
2020-03-03 11:43:00 +0900

    main

--- run.py
+++ run.py
@@ -18,5 +18,20 @@ def odd_even():
         <input name="num"></input>
         </form>""".format(str(request.form["num"]), ["偶数", "奇数"][int(request.form["num"]) % 2])
 
+@app.route("/odd", methods=["GET", "POST"])
+def odd_even():
+    if request.method == "GET":
+        return """
+        下に整数を入力してください。奇数か偶数か判定します
+        <form action="/" method="POST">
+        <input name="num"></input>
+        </form>"""
+    else:
+        return """
+        {}は{}です！
+        <form action="/" method="POST">
+        <input name="num"></input>
+        </form>""".format(str(request.form["num"]), ["偶数", "奇数"][int(request.form["num"]) % 2])
+
 if __name__ == "__main__":
     app.run(debug=True)
